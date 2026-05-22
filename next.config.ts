984c3f180adea17248fe766f0b44a13df5d6fe40
witt772 <134894729+witt772@users.noreply.github.com>
2026-05-22 09:36:41 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+    };
     return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
